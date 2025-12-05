2f38154bf67df3c3bab6681554eb8ce48f098c57
ellaisacoolkid <ellamcclain@seattleacademy.org>
2025-12-05 22:13:11 +0000

    Test

--- Projects/welcome_to_ct8.py
+++ Projects/welcome_to_ct8.py
@@ -27,4 +27,6 @@ print("\n\n")
                                                                                           
                                   
 print("Now it's your turn:")
-print("Find this line (line 30) in the welcome_to_ct8.py file, then change the message to say a fact about you.")                                  
+print ("Ive torn my acl")
+print ("ive torn my mcl")   
+print ("Ive torn my achilies")                               
---
+++ joke.py
@@ -0,0 +1,4 @@
+print("why can the toilet papernot cross the road")
+input("why")
+print("because it kept getting stuck in the crack")
+
